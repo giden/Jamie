3199cc97f0c04e3972aa4b151fafaa5c585fa056
Paweł Albecki <pawel.albecki@gmail.com>
2016-04-30 17:34:36 +0200

    add paginator

--- app/music/bands.component.ts
+++ app/music/bands.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from 'angular2/core';
+import { PAGINATION_DIRECTIVES } from 'ng2-bootstrap/ng2-bootstrap';
 
 import { BandsService } from '../service/bands.service';
 import { Band } from '../entity/band';
 
 @Component({
     templateUrl: 'app/music/bands.component.html',
+    directives: [PAGINATION_DIRECTIVES],
     providers: [BandsService]
 })
 export class BandsComponent implements OnInit {
@@ -12,16 +14,21 @@ export class BandsComponent implements OnInit {
     bands: Array<Band>;
     bands_error: boolean = false;
 
+    totalItems: number = 30;
+    currentPage: number = 1;
+    itemsPerPage: number = 3;
+
+
     constructor(private _bandsService: BandsService) { }
 
     ngOnInit() {
         this.getBands();
     }
 
-    getBands() {
+    getBands(event:any = {page: this.currentPage, itemsPerPage: this.itemsPerPage}) {
         var params: string = '';
-        params += '&offset=0';
-        params += '&limit=3';
+        params += '&offset='+event.page*3;
+        params += '&limit='+event.itemsPerPage;
         params += '&order=popularity_month';
         params += '&hasimage=1';
         console.log(params);
